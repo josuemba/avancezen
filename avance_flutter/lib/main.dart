876e75cbe7ab45b4054d95b5d1ce59b1e47cc75b
josuemba <josue.mba@gmail.com>
2024-04-19 21:23:47 -0600

    Actualizados

--- avance_flutter/lib/main.dart
+++ avance_flutter/lib/main.dart
@@ -7,8 +7,11 @@ import 'package:serverpod_flutter/serverpod_flutter.dart';
 // The client is set up to connect to a Serverpod running on a local server on
 // the default port. You will need to modify this to connect to staging or
 // production servers.
-var client = Client('http://$localhost:8080/')
-  ..connectivityMonitor = FlutterConnectivityMonitor();
+
+//var client = Client('https://$localhost:8080/')
+//  ..connectivityMonitor = FlutterConnectivityMonitor();
+String host = 'https://api.avancezen.com:8080/';
+var client = Client(host)..connectivityMonitor = FlutterConnectivityMonitor();
 
 void main() {
   runApp(const MyApp());
